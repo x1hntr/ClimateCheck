db6e460ee7497f43dcddd5431782735b5fb3b6fd
Daniel_Villavicencio <x1hntr@gmail.com>
2020-04-08 18:45:25 -0500

    FirstCommit

---
+++ ClimateCheck/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+
+class App extends Component{
+  render(){
+    return(
+      
+    );
+  }
+} export default App;
---
+++ ClimateCheck/src/screens/home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+HomeScreen = () =>
+<View>
+    <Text>Hola</Text>
+</View>
+export default HomeScreen;
